09d37009083dc72eb756b5e481c091f200edb040
hxlfare <g.hxflare@gmail.com>
2026-05-28 19:25:21 +0300

    nothing in general

--- programs/clipboardlib.c
+++ programs/clipboardlib.c
@@ -11,16 +11,34 @@ struct clipboard {
   cstring_da elements;
   int cur;
 };
-int init_sock() {
+enum cb_req_type {
+  CHANGE_C,
+  ADD_NEW,
+  DEL_AT,
+  INSERT_AT,
+};
+struct cb_request {
+  enum cb_req_type type;
+  cstring req_str;
+  int req_int;
+};
+int init_server() {
+  int server_socket;
+
   struct sockaddr_un server_addr;
-  int sock_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+  int result;
+  server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
   server_addr.sun_family = AF_UNIX;
-  strcpy(server_addr.sun_path, CLIPBOARD_SOCK_NAME);
-  int result =
-      connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (result == 0) {
-    return sock_fd;
-  } else {
-    return -1;
+  strcpy(server_addr.sun_path, "unix_socket");
+  int slen = sizeof(server_addr);
+  result = bind(server_socket, (struct sockaddr *)&server_addr, slen);
+  return server_socket;
+}
+void start_listen(int fd) {
+  int client_socket;
+  struct sockaddr_un client_addr;
+  listen(fd, 1);
+  while (1) {
+    cstring text;
   }
 }
