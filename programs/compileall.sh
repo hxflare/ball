2a1fbbc9a2bc00a40b7207ab65ec5269f2c526d6
hxlfare <g.hxflare@gmail.com>
2026-06-04 22:28:35 +0300

    make compileall use the libs directory

--- programs/compileall.sh
+++ programs/compileall.sh
@@ -6,14 +6,15 @@ if [ ${#files[@]} -eq 0 ]; then
     echo "No .c files found in $DIR"
     exit 1
 fi
-BTOOLS="$(dirname "$0")/../btools.c"
+LIBSDIR="$(dirname "$0")/../libs"
+BTOOLS=($LIBSDIR/*.c)
 OUTDIR="$(dirname "$0")/../building/isoroot/initramfs/bin"
 echo "Cleaning old binaries in $DIR..."
 for file in "${files[@]}"; do
     name="${file%.c}"
     echo "Compiling $file -> $name"
-    rm $name
-    gcc -static -o "$name" "$file" "$BTOOLS" || { echo "Compilation failed: $file"; exit 1; }
+    rm -f "$name"
+    gcc -static -o "$name" "$file" "${BTOOLS[@]}" || { echo "WARNING: Compilation failed: $file (skipping)"; continue; }
 done
 echo "Wiping $OUTDIR."
 rm -rf "$OUTDIR"
