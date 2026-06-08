7d4003f0527d050329487f87b78950b4a7e106b0
hxlfare <g.hxflare@gmail.com>
2026-06-08 11:18:06 +0300

    looped cat

--- programs/cat.c
+++ programs/cat.c
@@ -1,38 +1,42 @@
+#include "../btools.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "../btools.h"
+
 int main(int argc, char **argv) {
-    char *dpath =argv[1];
-    if (dpath[0]=='/'){
-        char *path=malloc(strlen(dpath)+3);
-        snprintf(path, strlen(path)+3, "./%s", dpath);
-    }else {
-        char *path=dpath;
+  if (argc < 2) {
+    return 0;
+  }
+  for (int i = 1; i < argc; i++) {
+    char *dpath = argv[i];
+    FILE *fptr = fopen(dpath, "r");
+    if (!fptr) {
+      cprint("shi... failed to open file\n");
+      continue;
     }
     char *fullstr = NULL;
     char ch;
-    FILE *fptr = fopen(argv[1], "r");
-    if (!fptr) {
-        cprint("shi... failed to open file");
-        return 1;
-    }
     int index = 0;
     while ((ch = fgetc(fptr)) != EOF) {
-        fullstr = realloc(fullstr, index + 2);
-        if (!fullstr) {
-            cprint("dementia");
-            fclose(fptr);
-            return 1;
-        }
-        fullstr[index++] = ch;
+      char *tmp = realloc(fullstr, index + 2);
+      if (!tmp) {
+        cprint("dementia\n");
+        if (fullstr)
+          free(fullstr);
+        fclose(fptr);
+        return 1;
+      }
+      fullstr = tmp;
+      fullstr[index++] = ch;
     }
-    if (fullstr)
-        fullstr[index] = '\0';
-    if (fullstr)
-        cprint(fullstr);
-    free(fullstr);
+    if (fullstr) {
+      fullstr[index] = '\0';
+      cprint(fullstr);
+      free(fullstr);
+    }
+
     fclose(fptr);
-    return 0;
+  }
+  return 0;
 }
